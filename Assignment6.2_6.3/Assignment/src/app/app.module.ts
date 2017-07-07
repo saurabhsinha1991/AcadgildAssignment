3ed579b0346ac52d8d5079db8ef9f816b7012440
Saurabh Sinha <ssi253@sapient.com>
2017-07-07 14:54:01 +0530

    Assignment 6.2/6.3

--- Assignment6.2_6.3/Assignment/src/app/app.module.ts
+++ Assignment6.2_6.3/Assignment/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { MyNewDirectiveDirective } from './my-new-directive.directive';
 import { MyNewPipePipe } from './my-new-pipe.pipe';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewCmpComponent,
     MyNewDirectiveDirective,
-    MyNewPipePipe
+    MyNewPipePipe,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
